69f4dd1113a10cb606ff5a5c345df71aeab21351
Matteo Piccini <gnappo.1@live.it>
2022-11-23 16:19:21 -0800

    Add DOM events final code

--- DOM/intro/dom.js
+++ DOM/intro/dom.js
@@ -1,1 +1,18 @@
-console.log("Ciaooooo")
+const formDiv = document.getElementById("form")
+const peopleUl = document.querySelector("#people > ul")
+console.log(formDiv)
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     // alert("JS Events are fun, makle sure to check out this page: https://developer.mozilla.org/en-US/docs/Web/Events")
+// })
+
+formDiv.addEventListener("submit", (e) => {
+    e.preventDefault() // DO NOT REFRESH THE PAGE
+    // debugger
+    const name = e.target.name.value
+    const age = e.target.age.value
+    const newLi = document.createElement("li")
+    newLi.innerText = `${name} ${age}`
+    peopleUl.appendChild(newLi)
+    e.target.reset()
+})
