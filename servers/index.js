0fbae3ad1d4d16b2c342564d52cf97262be6eeb8
Matteo Piccini <gnappo.1@live.it>
2022-11-30 10:05:14 -0800

    Add end-of-chat code for fetch

--- servers/index.js
+++ servers/index.js
@@ -5,3 +5,23 @@
 // Update -> PATCH/PUT
 // Destroy -> DELETE
 
+// FETCH
+console.log("a") // SYNC
+// it creates an http async request
+// it returns a Promise object
+// 3 statuses: pending, fulfilled, rejected
+// 3 methods: #then, #catch, #finally
+fetch("http://localhost:3000/pokemons")
+.then(response => {
+    console.log("b") // ASYNC
+    return response.json()
+}) // JSON
+.then(data => {
+    console.log("c") // ASYNC
+
+    // data.forEach(createPokemonCard)
+})
+// .then()
+.catch((error) => alert(error))
+
+console.log("d") // SYNC
